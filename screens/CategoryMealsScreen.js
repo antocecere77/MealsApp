c88ed465ab6833fa5103936ac30053ccd0331ac7
antocecere <antocecere77@gmail.com>
2020-05-08 13:00:39 +0200

    Rendering a meals list

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = props => {
 
     const renderMealItem = itemData => {
         return (
-            <View>
-                <Text>{itemData.item.title}</Text>
-            </View>
+           <MealItem 
+                title={itemData.item.title} 
+                image={itemData.item.imageUrl}
+                duration={itemData.item.duration}
+                complexity={itemData.item.complexity}
+                affordability={itemData.item.affordability}
+                onSelectMeal={() => {}} 
+            />
         );
     }
 
@@ -24,7 +30,8 @@ const CategoryMealsScreen = props => {
             <FlatList 
                 data={displayedMeals}
                 keyExtractor={(item, index) => item.id} 
-                renderItem={renderMealItem}/>
+                renderItem={renderMealItem}
+                style={{width: '100%'}}/>
         </View>
     );
 };
